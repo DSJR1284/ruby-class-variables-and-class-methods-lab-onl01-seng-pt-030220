cddfcb47e6ecae015043e0397dec431144c15f62
Doug Jones <dsjr1284@gmail.com>
2020-03-28 15:03:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,13 +5,13 @@ class Song
 
 def initialize(name, artist, genre)
   @@count += 1
-  @name = name 
-  @artist = artist 
-  @genre = genre 
+  @name = name
+  @artist = artist
+  @genre = genre
 end
 
 
 def self.count
   @@count
 end
-end 
+end
