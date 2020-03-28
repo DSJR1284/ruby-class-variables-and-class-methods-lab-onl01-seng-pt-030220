9277713811fb59c648f7ee1ecd6e812b4b28aa52
Doug Jones <dsjr1284@gmail.com>
2020-03-28 16:24:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,6 @@ class Song
   @@genres=[]
   @@artists=[]
 
-
-
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
@@ -36,7 +34,18 @@ class Song
       genre_count
     end
 
-
+    def self.artist_count
+      artist_count={}
+      @@artists.each do|artist|
+        if artist_count[artist]
+           artist_count[artist]+= 1
+         else
+           artist_count[artist] = 1
+        end
+      end
+      artist_count
+    end
+    
       def self.count
         @@count
       end
